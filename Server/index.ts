79b14adacffe4fb1e8a0656b4d30b470d9100b9b
Adrian <akirchner@hawk.iit.edu>
2021-02-23 21:28:36 -0600

    fixed ssl problem by changing from https to http

--- Server/index.ts
+++ Server/index.ts
@@ -4,5 +4,5 @@ const app = express();
 const PORT = 8000;
 app.get('/', (req, res) => res.send('Express + TypeScript Server'));
 app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
