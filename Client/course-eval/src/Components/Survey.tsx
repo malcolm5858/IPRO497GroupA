55e06f36a17acfb0730053f120fd7087040b0998
Malcolm <malcolm9292@gmail.com>
2021-03-04 21:22:40 -0600

    fixed formatting and http request

--- Client/course-eval/src/Components/Survey.tsx
+++ Client/course-eval/src/Components/Survey.tsx
@@ -4,17 +4,23 @@ import { Button, FormGroup } from "semantic-ui-react";
 import { Radio, TextArea, Form } from "formik-semantic-ui-react";
 
 interface Values {
-  courseSelection: string;
+  professor_rating: string;
   courseComments: string;
-  materialSelection: string;
+  class_rating: string;
   materialComments: string;
+}
+
+interface Result {
+  professor_rating: string;
+  class_rating: string;
+  default_questions_responses: [string, string];
 }
 
 export const Survey: React.FC = () => {
   const initialValues: Values = {
-    courseSelection: "",
+    professor_rating: "",
     courseComments: "",
-    materialSelection: "",
+    class_rating: "",
     materialComments: "",
   };
   return (
@@ -27,6 +33,16 @@ export const Survey: React.FC = () => {
         onSubmit={(data, { setSubmitting }) => {
           setSubmitting(true);
           // make async call
+          let questions: [string, string] = [
+            data.materialComments,
+            data.courseComments,
+          ];
+          let result: Result = {
+            professor_rating: data.professor_rating,
+            class_rating: data.class_rating,
+            default_questions_responses: questions,
+          };
+          result.class_rating = data.class_rating;
 
           fetch("http://localhost:8000/surveyResponse", {
             method: "POST",
@@ -34,25 +50,25 @@ export const Survey: React.FC = () => {
               Accept: "application/json",
               "Content-Type": "application/json",
             },
-            body: JSON.stringify(data),
+            body: JSON.stringify(result),
           });
           console.log("submit: ", data);
           setSubmitting(false);
         }}>
         {({ values, handleSubmit, isSubmitting }) => (
           <Form onSubmit={handleSubmit}>
-            <h3>Instructor</h3>
-            <div>
+            <h3 style={{ paddingLeft: 20 }}>Instructor</h3>
+            <div style={{ paddingLeft: 20 }}>
               <FormGroup inline>
                 <label>
                   On a scale from 1 to 5, how satisfied are
                   <br /> you with your instructor for this course?
                 </label>
-                <Radio name="courseSelection" value="1" label="1" />
-                <Radio name="courseSelection" value="2" label="2" />
-                <Radio name="courseSelection" value="3" label="3" />
-                <Radio name="courseSelection" value="4" label="4" />
-                <Radio name="courseSelection" value="5" label="5" />
+                <Radio name="professor_rating" value="1" label="1" />
+                <Radio name="professor_rating" value="2" label="2" />
+                <Radio name="professor_rating" value="3" label="3" />
+                <Radio name="professor_rating" value="4" label="4" />
+                <Radio name="professor_rating" value="5" label="5" />
               </FormGroup>
             </div>
             <TextArea
@@ -60,18 +76,20 @@ export const Survey: React.FC = () => {
               style={{ minHeight: 100 }}
               name="courseComments"
             />
-            <h3>Course</h3>
-            <FormGroup>
-              <label>
-                On a scale from 1 to 5, how satisfied are <br />
-                you with the course material?
-              </label>
-              <Radio name="materialSelection" label="1" value="1" />
-              <Radio name="materialSelection" label="2" value="2" />
-              <Radio name="materialSelection" label="3" value="3" />
-              <Radio name="materialSelection" label="4" value="4" />
-              <Radio name="materialSelection" label="5" value="5" />
-            </FormGroup>
+            <h3 style={{ paddingLeft: 20 }}>Course</h3>
+            <div style={{ paddingLeft: 20 }}>
+              <FormGroup inline>
+                <label>
+                  On a scale from 1 to 5, how satisfied are <br />
+                  you with the course material?
+                </label>
+                <Radio name="class_rating" label="1" value="1" />
+                <Radio name="class_rating" label="2" value="2" />
+                <Radio name="class_rating" label="3" value="3" />
+                <Radio name="class_rating" label="4" value="4" />
+                <Radio name="class_rating" label="5" value="5" />
+              </FormGroup>
+            </div>
             <TextArea
               placeholder="Input text here"
               style={{ minHeight: 100 }}
@@ -85,7 +103,7 @@ export const Survey: React.FC = () => {
               type="submit">
               Submit
             </Button>
-            <pre>{JSON.stringify(values, null, 2)}</pre>
+            {/* <pre>{JSON.stringify(values, null, 2)}</pre> */}
           </Form>
         )}
       </Formik>
