fc4cb100f44e5479211a25cdb6d6a0e3daafc435
Adrian <akirchner@hawk.iit.edu>
2021-03-16 18:43:32 -0500

    formatting is trash, but rating aggregation should (might) work (completely untested) (probably fine)

--- Client/course-eval/src/App.tsx
+++ Client/course-eval/src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Survey } from "./Components/Survey";
 import Home from "./Pages/Home";
+import { TeacherRatings } from "./Pages/TeacherRatings";
 
 function App() {
-  return <Home />;
+  return <TeacherRatings professor_id="60510944bb729c5f71c95714" professor_name="Bob" />;
 }
 
 export default App;
--- Client/course-eval/src/Pages/TeacherRatings.tsx
+++ Client/course-eval/src/Pages/TeacherRatings.tsx
@@ -1,8 +1,27 @@
-import React, from 'react';
+import React, { useState } from 'react';
 import Rating from '../Components/Rating';
 
-export function TeacherRatings() {
+export function TeacherRatings(props: any) {
+    const { professor_id, professor_name } = props;
+
+    const [ratings, setRatings] = useState([]);
+    const arrAvg = (arr : number[]) => arr.reduce((a : number, b : number) => a + b, 0) / arr.length
+
+    const getRatings = async () => {
+        try {
+            const response = await fetch(`http://localhost:8000/teacherResults/${professor_id}`)
+            const jsonData = await response.json();
+
+            setRatings(jsonData.responses);
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
     return (
-        <Rating size={500} rating={4.6} />
+        <>
+            <h1 className="align-center" >{professor_name}</h1>
+            <Rating size={200} rating={arrAvg(ratings.map((a: {professor_rating : number}) => a.professor_rating))} />
+        </>
     )
 }
