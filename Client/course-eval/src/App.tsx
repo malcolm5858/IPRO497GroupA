6b4783997f426df048bb0b12a35ab2ff171d5a5e
Adrian <akirchner@hawk.iit.edu>
2021-03-16 18:43:32 -0500

    formatting is trash, but rating aggregation should (might) work (completely untested) (probably fine)

--- Client/course-eval/src/App.tsx
+++ Client/course-eval/src/App.tsx
@@ -2,29 +2,13 @@ import React from "react";
 import { HistogramWithData } from "./Components/HistogramWithData";
 import { Survey } from "./Components/Survey";
 import Home from "./Pages/Home";
+import { TeacherRatings } from "./Pages/TeacherRatings";
 
 function App() {
   return (
-    <HistogramWithData
-      title="Class Average"
-      data={[
-        { term: "Fall 2020", Rating: 4.5 },
-        { term: "Spring 2020", Rating: 4.3 },
-        { term: "Fall 2021", Rating: 2.2 },
-        { term: "Spring 2022", Rating: 2.2 },
-        { term: "Fall 2023", Rating: 5.0 },
-        { term: "Spring 2023", Rating: 4.3 },
-        { term: "Fall 2024", Rating: 2.2 },
-        { term: "Spring 2024", Rating: 2.2 },
-        { term: "Fall 2025", Rating: 5.0 },
-        { term: "Spring 2025", Rating: 4.3 },
-        { term: "Fall 2026", Rating: 2.2 },
-        { term: "Spring 2026", Rating: 2.2 },
-        { term: "Fall 2027", Rating: 5.0 },
-        { term: "Spring 2027", Rating: 4.3 },
-        { term: "Fall 2028", Rating: 2.2 },
-        { term: "Spring 2028", Rating: 2.2 },
-      ]}
+    <TeacherRatings
+      professor_id="60510944bb729c5f71c95714"
+      professor_name="Bob"
     />
   );
 }
--- Client/course-eval/src/Components/Rating.tsx
+++ Client/course-eval/src/Components/Rating.tsx
@@ -1,21 +1,43 @@
-import React, { Fragment } from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
 import '../css/Rating.css';
 
 const Rating = (props: any) => {
     const {size, rating} = props;
+    const [offset, setOffset] = useState(0);
 
     const center = size / 2;
     const radius = size * 5 / 12;
     const width = size / 6;
     const circumference = 2 * Math.PI * radius;
 
+    useEffect(() => {
+        const ratingOffset = ((5 - rating) / 5) * circumference;
+        setOffset(ratingOffset);
+    }, [setOffset, circumference, rating, offset]);
     
     return (
         <Fragment>
             <svg className="svg" width={size} height={size}>
-                <circle className="svg-circle-bg" stroke='#666666' cx={center} cy={center} r={radius} strokeWidth={width} />
-                <circle className="svg-circle" stroke='#ffc821' cx={center} cy={center} r={radius} strokeWidth={width} />
-                <text className="svg-circle-text" x={center} y={center}>{rating.toFixed(1)}</text>
+                <circle
+                    className="svg-circle-bg"
+                    stroke='#cccccc'
+                    cx={center}
+                    cy={center}
+                    r={radius}
+                    strokeWidth={width}
+                />
+                <circle
+                    className="svg-circle"
+                    stroke='#ffc821'
+                    cx={center}
+                    cy={center}
+                    r={radius}
+                    strokeWidth={width}
+                    strokeDasharray={circumference}
+                    strokeDashoffset={offset}
+                />
+
+                <text className="svg-circle-text" x={`${center}`} y={`${center}`}>{rating.toFixed(1)}</text>
             </svg>
         </Fragment>
     );
