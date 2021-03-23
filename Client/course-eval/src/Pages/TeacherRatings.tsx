7bddb964b80dd015462a3ecec8dcb1bdb94f858f
Adrian <akirchner@hawk.iit.edu>
2021-03-23 17:05:31 -0500

    overall rating formatting fixed up a bit

--- Client/course-eval/src/Pages/TeacherRatings.tsx
+++ Client/course-eval/src/Pages/TeacherRatings.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import Rating from '../Components/Rating';
 
 export function TeacherRatings(props: any) {
@@ -20,9 +21,16 @@ export function TeacherRatings(props: any) {
 
     return (
         <>
-            <h1 className="align-center" >{professor_name}</h1>
-            {/* <Rating size={200} rating={arrAvg(ratings.map((a: {professor_rating : number}) => a.professor_rating))} /> */}
-            <Rating size={200} rating={4.6} />
+            <Container>
+                <Row>
+                    <Col>
+                        <p style={{fontSize: 40, fontWeight: 'bold', textAlign: 'center' }}>{professor_name}</p>
+                        <Rating size={200} rating={arrAvg(ratings.map((a: {professor_rating : number}) => a.professor_rating))} />
+                        <p style={{textAlign: 'center', fontSize: 24}}>Overall Rating</p>
+                        {/* <Rating size={200} rating={4.6} /> */}
+                    </Col>
+                </Row>
+            </Container>
         </>
     )
 }
