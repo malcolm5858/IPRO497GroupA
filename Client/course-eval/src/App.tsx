05ec2b45512425eef3318e35a862873aac6d02cf
Malcolm <malcolm9292@gmail.com>
2021-03-23 23:46:02 -0500

    Added a react router

--- Client/course-eval/src/App.tsx
+++ Client/course-eval/src/App.tsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { HistogramWithData } from "./Components/HistogramWithData";
 import { Survey } from "./Components/Survey";
 import Home from "./Pages/Home";
 import { TeacherRatings } from "./Pages/TeacherRatings";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Switch>
+        <Route
+          path="/Survey/:studentId/:name/:surveyId/:className"
+          children={<Survey />}
+        />
+        <Route exact path="/" children={<Home />} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
