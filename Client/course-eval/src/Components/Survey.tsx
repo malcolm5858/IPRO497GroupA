3aacf8dcc5cac074b7a30e1f75a0c257c5f32c97
Adrian <akirchner@hawk.iit.edu>
2021-04-20 17:04:28 -0500

    Cleaned up survey page formatting and added a background color

--- Client/course-eval/src/Components/Survey.tsx
+++ Client/course-eval/src/Components/Survey.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import { Formik } from "formik";
 import { Button, FormGroup } from "semantic-ui-react";
 import { Radio, TextArea, Form } from "formik-semantic-ui-react";
+import { Container, Row, Col } from "react-bootstrap";
 import { useParams } from "react-router";
+import "../css/Survey.css";
 import _ from "lodash";
 
 interface Values {
@@ -32,91 +34,99 @@ export const Survey: React.FC = () => {
     prof_comments: "",
     class_comments: "",
   };
+
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>{className}</h1>
       <h2 style={{ textAlign: "center", color: "grey" }}>{name}</h2>
 
-      <Formik
-        initialValues={initialValues}
-        onSubmit={(data, { setSubmitting }) => {
-          setSubmitting(true);
-          // make async call
-          let default_questions_responses = [
-            data.prof_comments,
-            data.class_comments,
-          ];
-          let output = {
-            survey_id: data.survey_id,
-            student_id: data.student_id,
-            professor_rating: data.professor_rating,
-            class_rating: data.class_rating,
-            default_questions_responses: default_questions_responses,
-            professor_made_questions_responses: [],
-          };
+      <Container>
+        <Row>
+          <Col>
+            <Formik
+              initialValues={initialValues}
+              onSubmit={(data, { setSubmitting }) => {
+                setSubmitting(true);
+                // make async call
+                let default_questions_responses = [
+                  data.prof_comments,
+                  data.class_comments,
+                ];
+                let output = {
+                  survey_id: data.survey_id,
+                  student_id: data.student_id,
+                  professor_rating: data.professor_rating,
+                  class_rating: data.class_rating,
+                  default_questions_responses: default_questions_responses,
+                  professor_made_questions_responses: [],
+                };
 
-          fetch("http://localhost:8000/surveyResponse", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(output),
-          });
-          console.log("submit: ", output);
-          setSubmitting(false);
-        }}>
-        {({ values, handleSubmit, isSubmitting }) => (
-          <Form onSubmit={handleSubmit}>
-            <h3 style={{ paddingLeft: 20 }}>Instructor</h3>
-            <div style={{ paddingLeft: 20 }}>
-              <FormGroup inline>
-                <label>
-                  On a scale from 1 to 5, how satisfied are
+                fetch("http://localhost:8000/surveyResponse", {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify(output),
+                });
+                console.log("submit: ", output);
+                setSubmitting(false);
+              }}>
+              {({ values, handleSubmit, isSubmitting }) => (
+                <Form onSubmit={handleSubmit}>
+                  <h3>Instructor</h3>
+                  <div>
+                    <FormGroup inline>
+                      <label>
+                        On a scale from 1 to 5, how satisfied are
                   <br /> you with your instructor for this course?
                 </label>
-                <Radio name="professor_rating" value={1} label="1" />
-                <Radio name="professor_rating" value={2} label="2" />
-                <Radio name="professor_rating" value={3} label="3" />
-                <Radio name="professor_rating" value={4} label="4" />
-                <Radio name="professor_rating" value={5} label="5" />
-              </FormGroup>
-            </div>
-            <TextArea
-              placeholder="Input text here"
-              style={{ minHeight: 100 }}
-              name="prof_comments"
-            />
-            <h3 style={{ paddingLeft: 20 }}>Course</h3>
-            <div style={{ paddingLeft: 20 }}>
-              <FormGroup inline>
-                <label>
-                  On a scale from 1 to 5, how satisfied are <br />
+                      <Radio name="professor_rating" value={1} label="1" />
+                      <Radio name="professor_rating" value={2} label="2" />
+                      <Radio name="professor_rating" value={3} label="3" />
+                      <Radio name="professor_rating" value={4} label="4" />
+                      <Radio name="professor_rating" value={5} label="5" />
+                    </FormGroup>
+                  </div>
+                  <TextArea
+                    placeholder="Additional Comments"
+                    style={{ minHeight: 100 }}
+                    name="prof_comments"
+                  />
+                  <h3>Course</h3>
+                  <div>
+                    <FormGroup inline>
+                      <label>
+                        On a scale from 1 to 5, how satisfied are <br />
                   you with the course material?
                 </label>
-                <Radio name="class_rating" label="1" value={1} />
-                <Radio name="class_rating" label="2" value={2} />
-                <Radio name="class_rating" label="3" value={3} />
-                <Radio name="class_rating" label="4" value={4} />
-                <Radio name="class_rating" label="5" value={5} />
-              </FormGroup>
-            </div>
-            <TextArea
-              placeholder="Input text here"
-              style={{ minHeight: 100 }}
-              name="class_comments"
-            />
+                      <Radio name="class_rating" label="1" value={1} />
+                      <Radio name="class_rating" label="2" value={2} />
+                      <Radio name="class_rating" label="3" value={3} />
+                      <Radio name="class_rating" label="4" value={4} />
+                      <Radio name="class_rating" label="5" value={5} />
+                    </FormGroup>
+                  </div>
+                  <TextArea
+                    placeholder="Additional Comments"
+                    style={{ minHeight: 100 }}
+                    name="class_comments"
+                  />
 
-            <Button
-              disabled={isSubmitting}
-              name="Submit"
-              id="button-submit"
-              type="submit">
-              Submit
+                  <Button
+                    disabled={isSubmitting}
+                    name="Submit"
+                    id="button-submit"
+                    type="submit">
+                    Submit
             </Button>
-            {/* <pre>{JSON.stringify(values, null, 2)}</pre> */}
-          </Form>
-        )}
-      </Formik>
+                  {/* <pre>{JSON.stringify(values, null, 2)}</pre> */}
+                </Form>
+              )}
+            </Formik>
+          </Col>
+        </Row>
+      </Container>
+
     </div>
   );
 };
