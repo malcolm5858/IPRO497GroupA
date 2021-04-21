a8f79580c2982f60edf49e762607ed9ea204df29
Malcolm <malcolm9292@gmail.com>
2021-04-21 09:08:21 -0500

    Finished survey page

--- Client/course-eval/src/Components/Survey.tsx
+++ Client/course-eval/src/Components/Survey.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Formik } from "formik";
 import { Button, FormGroup } from "semantic-ui-react";
 import { Radio, TextArea, Form } from "formik-semantic-ui-react";
@@ -14,6 +14,7 @@ interface Values {
   prof_comments: string;
   class_rating: number;
   class_comments: string;
+  professor_made_questions_responses: string[];
 }
 
 interface RouteParams {
@@ -25,6 +26,8 @@ interface RouteParams {
 
 export const Survey: React.FC = () => {
   let { studentId, name, surveyId, className } = useParams<RouteParams>();
+  const [questions, setQuestions] = useState([]);
+  const [value, setValue] = useState(0);
 
   const initialValues: Values = {
     survey_id: surveyId,
@@ -33,7 +36,28 @@ export const Survey: React.FC = () => {
     class_rating: 0,
     prof_comments: "",
     class_comments: "",
+    professor_made_questions_responses: [],
   };
+
+  const getData = async () => {
+    try {
+      const response = await fetch(`http://localhost:8000/Survey/${surveyId}`);
+      const jsonData = await response.json();
+
+      const questions = await jsonData.responses[0].professor_made_questions;
+      console.log(questions);
+      setQuestions(questions);
+      console.log(questions);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    console.log("TEst");
+    getData();
+    setValue((value) => value + 1);
+  }, []);
 
   return (
     <div style={{ backgroundColor: "lightcyan", height: "100vh" }}>
@@ -58,7 +82,8 @@ export const Survey: React.FC = () => {
                   professor_rating: data.professor_rating,
                   class_rating: data.class_rating,
                   default_questions_responses: default_questions_responses,
-                  professor_made_questions_responses: [],
+                  professor_made_questions_responses:
+                    data.professor_made_questions_responses,
                 };
 
                 fetch("http://localhost:8000/surveyResponse", {
@@ -73,13 +98,12 @@ export const Survey: React.FC = () => {
               }}>
               {({ values, handleSubmit, isSubmitting }) => (
                 <Form onSubmit={handleSubmit}>
-                  <h3>Instructor</h3>
                   <div>
                     <FormGroup inline>
                       <label>
                         On a scale from 1 to 5, how satisfied are
-                  <br /> you with your instructor for this course?
-                </label>
+                        <br /> you with your instructor for this course?
+                      </label>
                       <Radio name="professor_rating" value={1} label="1" />
                       <Radio name="professor_rating" value={2} label="2" />
                       <Radio name="professor_rating" value={3} label="3" />
@@ -92,13 +116,13 @@ export const Survey: React.FC = () => {
                     style={{ minHeight: 100 }}
                     name="prof_comments"
                   />
-                  <h3>Course</h3>
+
                   <div>
                     <FormGroup inline>
                       <label>
                         On a scale from 1 to 5, how satisfied are <br />
-                  you with the course material?
-                </label>
+                        you with the course material?
+                      </label>
                       <Radio name="class_rating" label="1" value={1} />
                       <Radio name="class_rating" label="2" value={2} />
                       <Radio name="class_rating" label="3" value={3} />
@@ -112,13 +136,30 @@ export const Survey: React.FC = () => {
                     name="class_comments"
                   />
 
+                  {questions.map((question: string, index: number) => (
+                    <div>
+                      <FormGroup inline>
+                        <div>
+                          <label style={{ fontWeight: "bold" }}>
+                            {question}
+                          </label>
+                        </div>
+                      </FormGroup>
+                      <TextArea
+                        placeholder="Comments"
+                        style={{ minHeight: 100 }}
+                        name={"professor_made_questions_responses." + index}
+                      />
+                    </div>
+                  ))}
+
                   <Button
                     disabled={isSubmitting}
                     name="Submit"
                     id="button-submit"
                     type="submit">
                     Submit
-            </Button>
+                  </Button>
                   {/* <pre>{JSON.stringify(values, null, 2)}</pre> */}
                 </Form>
               )}
@@ -126,7 +167,6 @@ export const Survey: React.FC = () => {
           </Col>
         </Row>
       </Container>
-
     </div>
   );
 };
